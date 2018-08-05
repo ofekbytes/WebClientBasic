cb0ad623d9a6e435e9628534562311b47c50cf01
Yaron Kessler <Yaron.Kessler@brinks.com>
2018-08-05 19:10:23 +0300

    temp

---
+++ temp/SmallMethod.java
@@ -0,0 +1,19 @@
+import converting.Convert;
+
+public class SmallMethod 
+{
+
+	
+	public SmallMethod() 
+	{
+		Convert convert = new Convert();
+	}
+
+	
+	public static void main(String[] args) 
+	{
+		SmallMethod smallMethod = new SmallMethod();
+
+	}
+
+}
